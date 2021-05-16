9acce07e158345db936e6be45697427fbb1f7ad9
Charles Jekal <cjekal@highlighttech.com>
2021-05-15 23:42:20 -0700

    adding outputting to JSON

--- predict.py
+++ predict.py
@@ -9,7 +9,6 @@ import glob
 if __name__ == "__main__":
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
-    cfg.MODEL.DEVICE = "cpu"
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
     cfg.MODEL.WEIGHTS = "./model.pth"
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
@@ -21,5 +20,12 @@ if __name__ == "__main__":
     MetadataCatalog.get("my_dataset").thing_classes = ["Edge", "Weld"]
 
     predictor = BatchPredictor(cfg)
-    images = [cv2.imread(file) for file in glob.glob("data/inference/**/*.jpg", recursive=True)]
+    images = [cv2.imread(file) for file in
+              glob.glob("data/inference/**/*.jpg", recursive=True) +
+              glob.glob("data/inference/**/*.JPG", recursive=True) +
+              glob.glob("data/inference/**/*.png", recursive=True) +
+              glob.glob("data/inference/**/*.PNG", recursive=True)
+    ]
     outputs = predictor.predict(images)
+    with open("data/predictions.json", "w") as f:
+        json.dump(outputs, f, indent=2)

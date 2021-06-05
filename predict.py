3bd9f9d98ef8737d8d564fbaa8413c15fa095feb
Charles Jekal <cjekal@highlighttech.com>
2021-06-04 22:09:16 -0700

    continued with working on batch prediction

--- predict.py
+++ predict.py
@@ -6,32 +6,25 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 import cv2
 import json
 import glob
+import os
 
-if __name__ == "__main__":
-    cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
-    cfg.MODEL.WEIGHTS = "./model.pth"
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
-    def get_dataset_catalog():
-        return []
+def get_predictions_filename(image):
+    filename_without_ext = os.path.splitext(os.path.basename(image))[0]
+    return f"data/predictions/edges-and-welds/{filename_without_ext}.json"
 
-    thing_classes = ["Edge", "Weld"]
-    DatasetCatalog.register("my_dataset", get_dataset_catalog)
-    MetadataCatalog.get("my_dataset").thing_classes = thing_classes
+def image_is_unprocessed(image):
+    predictions_file = get_predictions_filename(image)
+    return not os.path.exists(predictions_file)
 
-    predictor = BatchPredictor(cfg)
-    images = [cv2.imread(file) for file in
-              glob.glob("data/inference/**/*.jpg", recursive=True) +
-              glob.glob("data/inference/**/*.JPG", recursive=True) +
-              glob.glob("data/inference/**/*.png", recursive=True) +
-              glob.glob("data/inference/**/*.PNG", recursive=True)
-    ]
-    outputs = predictor.predict(images)
+def process_chunk(chunk, predictor):
+    outputs = predictor.predict(chunk)
     
-    predictions = []
-    for image, output in zip(images, outputs):
+    for image, output in zip(chunk, outputs):
         instances = output['instances'].to("cpu")
         num_instances = len(instances)
         image_size = instances.image_size
@@ -52,15 +45,54 @@ if __name__ == "__main__":
                 "pred_class": thing_classes[pred_class],
                 "pred_mask": pred_mask
             })
-        predictions.append({
+        predictions = {
             "image": image,
             "image_size": image_size,
             "num_instances": num_instances,
-            "instances":json_instances
-        })
-    
-    print("about to serialize:")
-    print(json.dumps(predictions, indent=2))
+            "instances": json_instances
+        }
+        print("about to serialize:")
+        print(json.dumps(predictions, indent=2))
 
-    with open("data/predictions.json", "w") as f:
-        json.dump(predictions, f, indent=2)
+        with open(get_predictions_filename(image), "w") as f:
+            json.dump(predictions, f, indent=2)
+    print("done processing chunk")
+
+if __name__ == "__main__":
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+    cfg.MODEL.WEIGHTS = "./model.pth"
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+
+    def get_dataset_catalog():
+        return []
+
+    thing_classes = ["Edge", "Weld"]
+    DatasetCatalog.register("my_dataset", get_dataset_catalog)
+    MetadataCatalog.get("my_dataset").thing_classes = thing_classes
+
+    predictor = BatchPredictor(cfg)
+    images = [cv2.imread(file) for file in
+              glob.glob("data/inference/*.jpg", recursive=True) +
+              glob.glob("data/inference/*.JPG", recursive=True) +
+              glob.glob("data/inference/*.png", recursive=True) +
+              glob.glob("data/inference/*.PNG", recursive=True)
+    ]
+    images = [i for i in images if image_is_unprocessed(i)]
+    outputs = predictor.predict(images)
+    
+
+"""
+pseudo-code:
+do the following once per model type (corrosion vs edges/welds)
+1. find images to process (those with missing .json prediction files)
+1. batch into 100 images at a time
+1. for each batch:
+    1. run batch prediction
+    1. for each prediction:
+        1. output to individual .json prediction file with same filename as source image
+
+after all model types are run, perform merging of .json prediction files
+1. for each image, output Visualizer output image with no scaling
+"""

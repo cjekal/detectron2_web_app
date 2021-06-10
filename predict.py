3ec05775e4a030015cad0602e03d9eaf6ac8c014
Charles Jekal <cjekal@highlighttech.com>
2021-06-09 23:30:54 -0700

    utilizing metadata to allow for corrosion and edges/welds support

--- predict.py
+++ predict.py
@@ -7,6 +7,7 @@ import cv2
 import glob
 import os
 import joblib
+import sys
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
@@ -14,8 +15,9 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 def get_predictions_filename(image):
+    global metadata
     filename_without_ext = os.path.splitext(os.path.basename(image))[0]
-    return f"data/predictions/edges-and-welds/{filename_without_ext}.pkl"
+    return f"data/predictions/{metadata['name']}/{filename_without_ext}.pkl"
 
 def image_is_unprocessed(image):
     predictions_file = get_predictions_filename(image)
@@ -33,6 +35,18 @@ def process_chunk(chunk, predictor):
     print("done processing chunk")
 
 if __name__ == "__main__":
+    global metadata
+    metadata = {
+        "corrosion": {
+            "name": "corrosion",
+            "thing_classes": ["Edge", "Weld"]
+        },
+        "edges-and-welds": {
+            "name": "edges-and-welds",
+            "thing_classes": ["Corrosion", "Light Corrosion"]
+        }
+    }[sys.argv[1]]
+
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
@@ -42,7 +56,7 @@ if __name__ == "__main__":
     def get_dataset_catalog():
         return []
 
-    thing_classes = ["Edge", "Weld"]
+    thing_classes = metadata["thing_classes"]
     DatasetCatalog.register("my_dataset", get_dataset_catalog)
     MetadataCatalog.get("my_dataset").thing_classes = thing_classes
 

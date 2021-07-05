00085bb8f16a78d928b5b8aa1b79157d4ed8e941
Charles Jekal <cjekal@highlighttech.com>
2021-07-04 23:49:00 -0700

    updating the merge behavior to output the JSON as well

--- merge.py
+++ merge.py
@@ -1,6 +1,7 @@
 import os
 import joblib
 import glob
+import json
 from detectron2.structures.instances import Instances
 
 def zip_by_pattern(corrosion_prediction_filenames, edges_welds_predictions_filenames):
@@ -25,6 +26,25 @@ def merge(prediction_pair):
 def get_merged_filename(merged_prediction):
     return os.path.join(f"data/predictions/merged/{merged_prediction['prediction_pair']['base_file']}")
 
+def print_bbox_json(merged_prediction):
+    json_prediction = {"prediction_pair": merged_prediction["prediction_pair"]}
+    json_prediction["instances"] = [get_dict_for_instance(instance) for instance in merged_prediction["instances"]]
+    print(json.dumps(json_prediction, indent=2))
+    print("---")
+
+def get_dict_for_instance(instance):
+    if len(instance.pred_boxes) > 1:
+        raise Exception("more than 1 pred boxes")
+    classes = ["Coating Corrosion", "Edge", "Weld"]
+    pred_class = instance.pred_classes[0].item()
+    bbox = instance.pred_boxes.tensor[0].tolist()
+    center = instance.pred_boxes.get_centers()[0].tolist()
+    return {
+        "pred_class": pred_class,
+        "bbox": bbox,
+        "bbox_center": center
+    }
+
 if __name__ == "__main__":
     os.makedirs("data/predictions/merged", exist_ok=True)
 
@@ -33,3 +53,4 @@ if __name__ == "__main__":
         merged_prediction = merge(prediction_pair)
         with open(get_merged_filename(merged_prediction), "wb") as f:
             joblib.dump(merged_prediction, f)
+        print_bbox_json(merged_prediction)
